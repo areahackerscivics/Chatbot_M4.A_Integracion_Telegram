990dccfa459e97518fa2fca88113d12f1e65eb59
Arnau Campos <arnauc6@gmail.com>
2017-06-28 10:03:53 +0200

    Limpieza código

--- telegram.py
+++ telegram.py
@@ -22,32 +22,24 @@ from textos import *
 ##---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 try:
     TOKEN = os.environ['BOT_ACCESS_TOKEN']
-    # activo = os.environ['activo']
-
 except:
     print "Error al cargar token de Telegram"
 
 
-# TECLADOS -----------------------------------------------------------------------------------------------------------------------------------------------------------
+# TECLADOS ---------------------------------------------------------------------
 tecladoIdioma = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Valencià', callback_data='Val'),
      InlineKeyboardButton(text='Castellano', callback_data='Cast')],#Dentro de la lista para que se vea en la misma línea
     ])
+# --------------------------------------------------------------------- TECLADOS
 
-# Diccionario para hacer el Case en Python --------------------------------------------------------------------------------------------------------------------------
-# Cada nuevo boton añadir un key: <nombre_función>
-funcionBoton = { # Las funciones se encuentran en: "from botonesTeclados import *"
+# CASE -------------------------------------------------------------------------
+# Diccionario para hacer el Case en Python (Cada nuevo boton añadir un key: <nombre_función>)
+funcionBoton = {        # Las funciones se encuentran en: "from botonesTeclados import *"
     "Cast": idiomaCast,
     "Val": idiomaVal
-    } # Tiene que estar al final de las funciones que pretendemos llamar
-
-# TEXTOS --------------------------------------------------------------------------------------------------------------------------------------------------
-# botInactivo = "¡Huy, ahora mismo estoy en mantenimiento! Vuelve a hablarme más tarde por favor."
-# comandoStart = 'Hola, es la primera vez que entras. ¿En qué idioma quieres que me comunique contigo?'
-# usuarioNoGuardado = 'Hola parece que ha habido un error y no tengo almacenado en que idioma quieres que me comunique contigo. ¿Me lo podrías recordar?'
-# errorNoTexto = "Perdona pero no entiendo este tipo de mensajes."
-# comandoIdioma = "¿En qué idioma quieres que me comunique contigo a partir de ahora?"
-
+    }                   # Tiene que estar al final de las funciones que pretendemos llamar
+# ------------------------------------------------------------------------- CASE
 
 ##---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## Funciónes
@@ -57,6 +49,7 @@ funcionBoton = { # Las funciones se encuentran en: "from botonesTeclados import 
 def on_chat_message(msg):
     insertarMensaje(msg) # Insertamos el mensaje recibido en la BD
 
+    # EXTRACCIÓN INFORMACIÓN MENSAJE -------------------------------------------
     content_type, chat_type, chat_id = telepot.glance(msg)
                 # - content_type: tipo de contenido del mensaje (text, etc.)
                 # - chat_type: por ahora siempre es private
@@ -64,18 +57,20 @@ def on_chat_message(msg):
 
     idUsario = chat_id # Para usuarios normales el valor es el mismo
 
-    usuario = buscarUsuario(idUsario)
+    usuario = buscarUsuario(idUsario) # Consultamos los datos del usuario
 
-    if usuario == None: # Usuario no existe
+    if usuario == None: # USUARIO NO EXISTE
         if msg.has_key('from'):
             nombreUsuario = msg['from']['first_name'] # Guardamos la variable de el nombre del usuario que se conecta
-            # idUsario = msg['from']['id']
         else:
             nombreUsuario = 'Ciudadano'
+
         insertarNuevoUsuario(nombreUsuario,idUsario)
         bot.sendMessage(chat_id, busquedaTexto('usuarioNoGuardado','Val'))
         bot.sendMessage(chat_id, busquedaTexto('usuarioNoGuardado','Cast'), reply_markup=tecladoIdioma)
-    else: # Usuario existe
+        return # Fin
+
+    else: # USUARIO EXISTE
         actualizarUsuario(idUsario)
 
         if usuario.has_key('idioma') and usuario.has_key('nombre'):
@@ -84,8 +79,9 @@ def on_chat_message(msg):
         else:
             idioma = 'Cast'
             nombreUsuario = 'Ciudadano'
+    # ------------------------------------------- EXTRACCIÓN INFORMACIÓN MENSAJE
 
-    # COMANDOS -----------------------------------------------------------------------------------------------
+    # COMANDOS -----------------------------------------------------------------
     # Búsqueda si el mensaje es un comando
     if msg.has_key('entities') and msg.has_key('text'):
         entidades = msg['entities']
@@ -104,67 +100,30 @@ def on_chat_message(msg):
             insertarNuevoUsuario(nombreUsuario,idUsario)
             bot.sendMessage(chat_id, busquedaTexto('comandoStart','Val'))
             bot.sendMessage(chat_id, busquedaTexto('comandoStart','Cast'), reply_markup=tecladoIdioma)
-            return
+            return # Fin
         elif comando == '/idioma':
             bot.sendMessage(chat_id, busquedaTexto('comandoIdioma',idioma), reply_markup=tecladoIdioma)
-            return
+            return # Fin
         # elif comando == '': # Añadir comandos
-    # ----------------------------------------------------------------------------------------------- COMANDOS
+    # ----------------------------------------------------------------- COMANDOS
 
-    # ACTIVO -------------------------------------------------------------------------------------------------
+    # ACTIVO -------------------------------------------------------------------
     if activo == False:
         bot.sendMessage(chat_id, busquedaTexto('botInactivo',idioma))
-        return
-    # ------------------------------------------------------------------------------------------------- ACTIVO
+        return # Fin
+    # ------------------------------------------------------------------- ACTIVO
 
-    # Solo dejamos entrar los mensajes que son tipo text
-    if content_type == "text" and msg.has_key('text') and entidad != 'bot_command':
-        # Buscamos si el texto recibido es una entidad 'comando' y de ser así cual es
-        # if msg.has_key('entities'):
-        #     entidades = msg['entities']
-        #     for entidad in entidades:
-        #         if entidad['type'] == "bot_command":
-        #             comando = msg['text'][0:entidad['length']]
-        #             entidad="bot_command"
-        #             break
-        # else:
-        #     entidad = ''
-        #     comando = ''
-        # Si tiene remitente sacamos su nombre, si no le llamamos 'Ciudadano'
-        # if msg.has_key('from'):
-        #     nombreUsuario = msg['from']['first_name'] # Guardamos la variable de el nombre del usuario que se conecta
-        #     # idUsario = msg['from']['id']
-        # else:
-        #     nombreUsuario = 'Ciudadano'
-            # idUsario = 0
-        # # COMANDOS -----------------------------------------------------------------------------------------------
-        # if entidad == 'bot_command':
-        #     if comando == '/start':
-        #         insertarNuevoUsuario(nombreUsuario,idUsario)
-        #         bot.sendMessage(chat_id, comandoStart, reply_markup=tecladoIdioma)
-        #     elif comando == '/idioma':
-        #         bot.sendMessage(chat_id, comandoIdioma, reply_markup=tecladoIdioma)
-        #
-        #     # elif comando == '': # Añadir comandos
-        # # ----------------------------------------------------------------------------------------------- COMANDOS
-        # else:
-        # Extraemos el contenido del texto
-        texto = msg['text']
-
-        # usuario = [{u'fechaInsercion': datetime.datetime(2017, 6, 26, 11, 42, 53, 295000), u'fechaUltimoAcceso': datetime.datetime(2017, 6, 27, 11, 14, 25, 415000), u'_id': 192224003, u'nombre': u'Arnau', u'idioma': u'Val', u'numPreguntas': 2}]
-
-        # if usuario == None: # Usuario no existe
-        #     insertarNuevoUsuario(nombreUsuario,idUsario)
-        #     bot.sendMessage(chat_id, busquedaTexto('usuarioNoGuardado',idioma), reply_markup=tecladoIdioma)
-        # else: # Usuario existe
-        #     actualizarUsuario(idUsario)
-
+    # RESPUESTA ----------------------------------------------------------------
+    if content_type == "text" and msg.has_key('text') and entidad != 'bot_command': # ENTRADA TEXTUAL
+        texto = msg['text'] # Extraemos el contenido del texto
 
         respuesta = obtenerRespuesta(texto,chat_id) # Buscamos respuesta
 
         bot.sendMessage(chat_id, respuesta) # Enviamos respuesta
-    else:
+
+    else: # ENTRADA NO TEXTUAL
         bot.sendMessage(chat_id, busquedaTexto('errorNoTexto',idioma))
+    # ---------------------------------------------------------------- RESPUESTA
 
 
 # Si el mensaje recibido se tratara de un respuesta CALLBACK (de un teclado) -----------------------------------------------------------------------------------------
@@ -187,11 +146,11 @@ def on_inline_query(msg):
 ## Inicio bot
 ##---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-bot = telepot.Bot(TOKEN)    # Creamos el escuchador del bot con id=TOKEN
-##bot.message_loop(handle)  # Iniciamos el escuchador del bot
+bot = telepot.Bot(TOKEN)    # Creamos el escuchador del bot con el TOKEN
+
 bot.message_loop({'chat': on_chat_message,
                   'callback_query': on_callback_query,
-                  'inline_query':on_inline_query})
+                  'inline_query':on_inline_query}) # Iniciamos el escuchador del bot
 
 ##---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## Mantiene el programa ejecutándose
